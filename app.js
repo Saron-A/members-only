a09d2e37aaa81bbef8165ddf957dd15575e805cf
Saron <saronabebe2019@gmail.com>
2025-10-30 10:57:56 -0800

    Implement authentication using bcryptjs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const pool = require("./db/pool");
 // step 1: import and initialize session and passport
 const session = require("express-session");
 const passport = require("passport");
@@ -20,20 +21,53 @@ express.urlencoded({ extended: true }); // to parse form data
 express.request(
   session({ secret: "cats", resave: false, saveUninitialized: false })
 );
-passport.initialize();
-passport.session();
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Step 2 - continued
 passport.use(
   new localStrategy(async (username, password, done) => {
     try {
-      const hashedPassword = await bcrypt.hash(password, 10);
       //set up database first before implementing this part
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "User not found" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "User not found" });
+      }
+      return done(null, user);
     } catch (err) {
-      console.error(err);
+      return done(err);
     }
   })
 );
+
+//step 3 _ Serialize and deserialize user
+passport.serializeUser((user, done) => {
+  //during sign in
+  return done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  //during log in/ authentication
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
 
 //ejs -define view engine and views folder
 app.set("views", path.join(__dirname, "views"));
